b20c88867f06500c4f99df88d8da8d7918c5f137
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-01 11:06:28 -0300

    video 2.2

--- src/main/kotlin/br/com/alura/alugames/principal/testeBanco.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/testeBanco.kt
@@ -1,12 +1,11 @@
 package br.com.alura.alugames.principal
 
-import br.com.alura.alugames.dados.Banco
 import br.com.alura.alugames.dados.JogosDAO
 import br.com.alura.alugames.modelo.Jogo
 
 fun main() {
     val jogo = Jogo("The Last of Us Part I", "https://cdn.cloudflare.steamstatic.com/steam/apps/1888930/header.jpg?t=1686864554", 5.99,"Uma aventura pós-apocalíptica de sobrevivência em um mundo infestado por zumbis e facções em conflito.")
-    JogosDAO.adicionarJogo(jogo)
+//    JogosDAO.adicionarJogo(jogo)
 
     val listaJogos: List<Jogo> = JogosDAO.getJogos()
     println(listaJogos)
