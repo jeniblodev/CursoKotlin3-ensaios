58f449a0b2190f81ebf174ddbe82f6cb914d8bf1
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-02 11:12:12 -0300

    Preparando o ambiente

---
+++ src/main/kotlin/br/com/alura/alugames/dados/PlanosEntity.kt
@@ -0,0 +1,15 @@
+package br.com.alura.alugames.dados
+
+sealed class PlanoEntity(val tipo: String) {
+}
+
+
+class PlanoAvulsoEntity(tipo: String): PlanoEntity(tipo) {
+}
+
+class PlanoAssinaturaEntity(
+    tipo: String,
+    val mesalidade: Double,
+    val jogosIncluidos: Int,
+    val percentualDescontoReputacao: Double): PlanoEntity(tipo) {
+}
